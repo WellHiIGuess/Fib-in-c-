631553cedc680784d8fc79dbe37d1c7613126a18
WellHiIGuess <67477071+WellHiIGuess@users.noreply.github.com>
2022-08-09 13:28:43 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+
+int main() {
+    int length = 100;
+    std::vector<std::uint64_t> nums;
+
+    for (int i = 0; i < length; i++) {
+        if (nums.size() < 2) {
+            nums.push_back(1);
+        } else {
+            nums.push_back(nums[i - 2] + nums[i - 1]);
+        }
+
+        std::cout << nums[i] << "\n";
+    }
+}
